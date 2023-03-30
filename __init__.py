7d3013775987fa7c96b560337b89b0e38ce99c4e
MuhammadRRahman <83684383+MuhammadRRahman@users.noreply.github.com>
2023-03-30 10:37:59 +0100

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,23 @@
+# Only import the classes we want to be accessible to the user
+from .allpostproc import All_PostProc
+from .mdfields import *
+from .mdpostproc import *
+
+from .cfdfields import *
+from .cfdpostproc import *
+
+from .channelflowfields import *
+from .channelflowpostproc import *
+
+from .serial_cfdfields import *
+
+from .cplfields import *
+from .cplpostproc import *
+from .pplexceptions import *
+
+
+from .openfoamfields import *
+from .openfoampostproc import *
+
+from .lammpsfields import *
+from .lammpspostproc import *
